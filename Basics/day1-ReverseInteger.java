9f68af21fa55ea5af1a596e376c3646bd811062d
tarungithub7549 <tarunsharma7260@gmail.com>
2026-04-04 23:54:57 +0530

    Day 1: reverse a integer

---
+++ Basics/day1-ReverseInteger.java
@@ -0,0 +1,25 @@
+// Problem: Reverse Integer
+// Approach: Math (modulus + division)
+// Time: O(log n)
+// Space: O(1)
+// Key Idea: extract digit and build reverse
+
+class Solution {
+    public int reverse(int x) {
+
+        int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+
+            rev = rev * 10 + digit;
+        }
+
+        return rev;
+    }
+}
