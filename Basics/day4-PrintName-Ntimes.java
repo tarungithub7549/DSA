cdf23a272b3186469854693b0046ebea42951e0f
tarungithub7549 <tarunsharma7260@gmail.com>
2026-04-07 21:33:01 +0530

    Day 4: Print name N times using Recursion

---
+++ Basics/day4-PrintName-Ntimes.java
@@ -0,0 +1,20 @@
+// Problem: Print a given name n times using recursion
+// Platform: basic recursion
+// Approach: Recursive (increase count until it reaches n)
+// Time: O(n)
+// Space: O(n)  (due to recursion stack)
+// Key Idea: print name and recursively call function with count+1 until count == n
+
+class Solution{
+    public static void printName(String name, int n, int count){
+            
+            if (n == count) {
+                return;
+            }
+            System.out.println(name);
+    
+            printName(name, n, count+1);
+        }
+}
+
+
