c639ea295371c6faef5f5a426717d5a849562a4e
tarungithub7549 <tarunsharma7260@gmail.com>
2026-04-13 22:40:39 +0530

    finding single element in an array

---
+++ Array/day10-SingleElement-Array.java
@@ -0,0 +1,18 @@
+// Problem: Single Number
+// Platform: LeetCode (136)
+// Approach: Use XOR to cancel out duplicate elements.
+// Time: O(n)
+// Space: O(1)
+// Key Idea: XOR of same numbers = 0, so only unique number remains.
+
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+
+        for(int num : nums){
+            xor = xor ^ num;
+        }
+        return xor;
+    }
+} 
