467726fdaa0f1a4525248f56acaa3ebe6140b29c
tarungithub7549 <tarunsharma7260@gmail.com>
2026-04-29 23:29:09 +0530

    Day 24: solving daily temperature problem using nested for loop(brute force

--- Array/day24-Daily-Temperature.java
+++ Array/day24-Daily-Temperature.java
@@ -22,3 +22,19 @@ class Solution{
         return res;
     }
 }
+//---------------------------------------------------------------------------
+// class Solution {
+//     public int[] dailyTemperatures(int[] temperatures) {
+//         int len = temperatures.length;
+//         int[] res = new int[len];
+//         for(int i = 0; i < len; i++){
+//             for(int j = i + 1; j < len; j++){
+//                 if(temperatures[j] > temperatures[i]){
+//                     res[i] = j - i;
+//                     break;
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// }
